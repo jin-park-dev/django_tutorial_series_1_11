baf8303731a77896a8e304d7c33cce8769a7656b
Jin Park <jinparkhan+git@googlemail.com>
2017-10-25 16:51:25 +0100

    pre_save, post_save

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.db.models.signals import pre_save, post_save
 
+from .utils import unique_slug_generator
 # Create your models here.
 
 class RestaurantLocation(models.Model):
@@ -10,6 +12,31 @@ class RestaurantLocation(models.Model):
     timestamp       = models.DateTimeField(auto_now_add=True) # When first added
     updated         = models.DateTimeField(auto_now=True) # Last updated
     # my_date_field   = models.DateField(auto_now=False, auto_now_add=False)
+    slug            = models.SlugField(blank=True, null=True)
 
     def __str__(self):
         return self.name
+
+    @property
+    def title(self):
+        return self.name
+
+
+def rl_pre_save_receiver(sender, instance, *args, **kwargs):
+    # print('saving..')
+    # print(instance.timestamp)
+    if not instance.slug:
+        # instance.name = 'ANother title'
+        instance.slug = unique_slug_generator(instance)
+
+# def rl_post_save_receiver(sender, instance, created, *args, **kwargs):
+#     print('saved')
+#     print(instance.timestamp)
+#     if not instance.slug:
+#         instance.slug = unique_slug_generator(instance)
+#         instance.save()
+
+
+pre_save.connect(rl_pre_save_receiver, sender=RestaurantLocation)
+
+# post_save.connect(rl_post_save_receiver, sender=RestaurantLocation)
--- restaurants/views.py
+++ restaurants/views.py
@@ -109,12 +109,6 @@ class RestaurantListView(ListView):
 class RestaurantDetailView(DetailView):
     queryset = RestaurantLocation.objects.all()
 
-    def get_context_data(self, *args,**kwargs):
-        print(self.kwargs)
-        context = super(RestaurantDetailView, self).get_context_data(*args, **kwargs)
-        print(context)
-        return context
-
     def get_object(self, *args, **kwargs):
         rest_id = self.kwargs.get('rest_id')
         obj = get_object_or_404(RestaurantLocation, id=rest_id) # pk = rest_id is also same.
