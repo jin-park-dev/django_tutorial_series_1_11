8a9c2ba6f828b938eefa2b86974bf85e0ec1d687
Jin Park <jinparkhan+git@googlemail.com>
2017-10-31 13:56:06 +0000

    Done: 37 - Personalize Items (5:35:42)

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,7 +6,7 @@ from django.db.models import Q
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.views import View
-from django.views.generic import TemplateView, ListView, DetailView, CreateView
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView
 
 from .models import RestaurantLocation
 from .forms import ResaurantCreateForm, RestaurantLocationCreateForm
@@ -14,22 +14,15 @@ from .forms import ResaurantCreateForm, RestaurantLocationCreateForm
 # Create your views here.
 # function based view
 
-class RestaurantListView(ListView):
+class RestaurantListView(LoginRequiredMixin, ListView):
     def get_queryset(self):
-        slug = self.kwargs.get("slug")
-        print('Rest_list_view slug: '.format(slug))
-        if slug:
-            queryset = RestaurantLocation.objects.filter(
-                Q(category__iexact=slug) |
-                Q(category__icontains=slug)
-            )
-        else:
-            queryset = RestaurantLocation.objects.all()
+        return RestaurantLocation.objects.filter(owner=self.request.user)
+
+
+class RestaurantDetailView(LoginRequiredMixin, DetailView):
+    def get_queryset(self):
+        queryset = RestaurantLocation.objects.filter(owner=self.request.user)
         return queryset
-
-
-class RestaurantDetailView(DetailView):
-    queryset = RestaurantLocation.objects.all() # Can filter by user
 
 
 # This works with forms.ModelForm to auto do saving aspect
@@ -49,3 +42,21 @@ class RestaurantCreateView(LoginRequiredMixin, CreateView):
         context = super(RestaurantCreateView, self).get_context_data(*args, **kwargs)
         context['title'] = 'Add Restaurant'
         return context
+
+class RestaurantUpdateView(LoginRequiredMixin, UpdateView):
+    form_class = RestaurantLocationCreateForm
+    template_name = 'restaurants/detail-update.html'
+
+    # def form_valid(self, form):
+    #     instance = form.save(commit=False)
+    #     instance.owner = self.request.user
+    #     return super(RestaurantUpdateView, self).form_valid(form)
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(RestaurantUpdateView, self).get_context_data(*args, **kwargs)
+        name = self.get_object().name
+        context['title'] = f'Update Restaurant: {name}'
+        return context
+
+    def get_queryset(self):
+        return RestaurantLocation.objects.filter(owner=self.request.user)
