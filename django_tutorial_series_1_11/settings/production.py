9fd296b7cceb9b1b1a8c8da4874cf29460473aeb
Jin Park <jinparkhan+git@googlemail.com>
2017-12-04 09:35:47 +0000

    Going live prep

--- django_tutorial_series_1_11/settings/production.py
+++ django_tutorial_series_1_11/settings/production.py
@@ -101,10 +101,10 @@ WSGI_APPLICATION = 'django_tutorial_series_1_11.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 # add this
